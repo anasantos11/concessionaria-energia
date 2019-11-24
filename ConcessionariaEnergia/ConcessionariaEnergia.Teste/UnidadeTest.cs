6fd5b08999870b9e64646cbace15363efa20eef9
Ana Santos <anasantos1103@gmail.com>
2019-11-24 20:51:07 -0300

    Criado classe unidade com os casos de teste.

---
+++ ConcessionariaEnergia/ConcessionariaEnergia.Teste/UnidadeTest.cs
@@ -0,0 +1,53 @@
+﻿using ConcessionariaEnergia.Model;
+using NUnit.Framework;
+using System;
+
+namespace ConcessionariaEnergia.Teste
+{
+    public class UnidadeTest
+    {
+        [Test]
+        public void TestArmazenarContasMensais()
+        {
+            var conta = new Conta(100, 11, 2019);
+            var unidade = new Unidade(Guid.NewGuid());
+
+            unidade.AdicionarContaMensal(conta);
+
+            Assert.IsTrue(unidade.ContasMensais.Contains(conta));
+        }
+
+        [Test]
+        public void TestArmazenarContasMensaisComContaJaArmazenada()
+        {
+            var conta = new Conta(100, 11, 2019);
+            var unidade = new Unidade(Guid.NewGuid());
+
+            unidade.AdicionarContaMensal(conta);
+
+            Assert.IsTrue(unidade.ContasMensais.Contains(conta));
+            Assert.That(() => unidade.AdicionarContaMensal(conta),
+                Throws.TypeOf<ArgumentException>()
+                    .With.Message.EqualTo("A conta informada já existe na unidade."));
+        }
+
+        [Test]
+        public void TestArmazenarContasMensaisContaNaoInformada()
+        {
+            var unidade = new Unidade(Guid.NewGuid());
+            Assert.That(() => unidade.AdicionarContaMensal(null),
+                Throws.TypeOf<ArgumentException>()
+                    .With.Message.EqualTo("A conta não foi informada."));
+        }
+
+        [Test]
+        public void TestArmazenarIdentificadorUnico()
+        {
+            var identificadorUnico = Guid.NewGuid();
+
+            var unidade = new Unidade(identificadorUnico);
+
+            Assert.AreEqual(identificadorUnico, unidade.Identificador);
+        }
+    }
+}
---
+++ ConcessionariaEnergia/ConcessionariaEnergia/Model/Unidade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConcessionariaEnergia.Model
+{
+    public class Unidade
+    {
+        public Unidade(Guid identificador)
+        {
+            this.ContasMensais = new List<Conta>();
+            this.Identificador = identificador;
+        }
+
+        public List<Conta> ContasMensais { get; set; }
+        public Guid Identificador { get; }
+
+        public void AdicionarContaMensal(Conta conta)
+        {
+            if (conta == null)
+            {
+                throw new ArgumentException("A conta não foi informada.");
+            }
+
+            if (this.ContasMensais.Contains(conta))
+            {
+                throw new ArgumentException("A conta informada já existe na unidade.");
+            }
+
+            this.ContasMensais.Add(conta);
+        }
+    }
+}
